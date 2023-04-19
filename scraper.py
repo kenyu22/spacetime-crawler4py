010bc116e8b818093a199e495934e045954e33de
kenny yu <bravekenneth@gmail.com>
2023-04-19 14:16:24 -0700

    added generated report, added tokenizer for word list

--- scraper.py
+++ scraper.py
@@ -3,6 +3,9 @@ from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
 WORD_THRESHOLD = 300
+unique_links = set()
+max_word_link = ''
+max_words = 0
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -27,11 +30,24 @@ def extract_next_links(url, resp):
     if len(soup.get_text()) <= WORD_THRESHOLD:
         return list()
 
+    words = [word.lower() for word in re.findall(r"[a-zA-Z][a-zA-Z0-9]*'?[a-zA-Z0-9]*", soup.get_text())]
+    num_words = len(words)
+    global max_words
+    if max_words is None:
+        max_words = num_words
+    elif max_words < num_words:
+        max_words = num_words
+        global max_word_link
+        max_word_link = url
+    
+    unique_links.add(url) # keep track of the valid links in a set
+
     urls = []
     for link in soup.find_all('a'): # retrieve all urls from the soup
         link = link.get('href')
         if link != None:
             urls.append(link.split('#')[0])
+            
         #print(urls[-1])
     
     return urls
@@ -68,3 +84,19 @@ def is_valid(url):
     except TypeError:
         print ("TypeError for ", parsed)
         raise
+
+def generate_report():
+    # generate the report with all questions from canvas and their corresponding answers
+    report = open('report.txt', 'w')
+
+    # Question 1
+    question_1 = ['1. How many unique pages did you find? \n', f'There are {len(unique_links)} unique links.\n\n']
+    # Question 2
+    question_2 = ['2. What is the longest page in terms of the number of words? \n', f'The longest page in terms of the number of words is {max_word_link}.\n\n']
+    # Question 3
+
+    # write answers to report file
+    for i in range(4):
+        report.writelines(f'question_{i+1}')
+    # close the report after all questions have been answered
+    report.close()
